344f876aec57e49925f3c5ae987d68b1a9456852
G Dinesh Varma <51696897+Dinesh123527@users.noreply.github.com>
2023-01-11 19:08:27 +0530

    added sql file

--- database/learnersPortal.sql
+++ database/learnersPortal.sql
@@ -1,24 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3307
--- Generation Time: Jun 04, 2021 at 09:32 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `administrative-portal`
+-- Database: `learnersPortal`
 --
 
 -- --------------------------------------------------------
